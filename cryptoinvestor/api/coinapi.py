f20c5c41199a81773ad585c4bbbddf5ccd03bd48
Marián Skrip <marian.skripp@gmail.com>
2018-04-06 17:26:47 +0200

    Finish CoinAPI integration

--- cryptoinvestor/api/coinapi.py
+++ cryptoinvestor/api/coinapi.py
@@ -13,11 +13,11 @@ logger = logging.getLogger(__name__)
 class Api(ApiBase):
     def __init__(self, config: dict):
         super(Api, self).__init__(config)
-        self.base = config.get('base_url', 'https://rest.coinapi.io/v1/')
 
-        self.sess.headers.update({
-            'X-CoinAPI-Key': config.get('api_key')
-        })
+        if config is None:
+            config = {}
+
+        self.base = config.get('base_url', 'https://rest.coinapi.io/v1/')
 
         if not config.get('api_key'):
             config['api_key'] = os.environ.get('COINAPI_KEY')
@@ -26,6 +26,10 @@ class Api(ApiBase):
             raise self.Error(
                 'Please provide CoinApi access key in env variable \'COINAPI_KEY\''
             )
+
+        self.sess.headers.update({
+            'X-CoinAPI-Key': config.get('api_key')
+        })
 
     def connect(self) -> bool:
         url = urljoin(self.base, 'exchanges')
@@ -62,17 +66,18 @@ class Api(ApiBase):
 
         return assets
 
-    def load(self, *, e_rate: Asset, base: str, time: datetime.datetime) -> Asset:
-        url = urljoin(self.base, f'exchangerate/{e_rate.id}/{base}')
-
+    def load(self, *, asset: Asset, base: str, time: datetime.datetime) -> Asset:
         payload = {
-            'time': time.isoformat()
+            'time':  time.strftime('%Y%m%dT%H%M%S')
         }
 
-        r = self.sess.get(url, data=payload)
+        url = urljoin(self.base, f'exchangerate/{asset.id}/{base}')
+
+        r = self.sess.get(url, params=payload)
+        print(r.url)
 
         if not r.ok:
-            self.error = f'Error loading data from {url}'
+            self.error = f'Error loading data from {url} request: {r.content}'
             return None
 
         data = r.json()
@@ -82,6 +87,18 @@ class Api(ApiBase):
         else:
             self.error = ''
 
-        e_rate.set_rate(data.get('asset_id_quote'), data.get('rate'), data.get('time'))
+        asset.set_rate(data.get('asset_id_quote'), data.get('rate'), data.get('time'))
 
-        return e_rate
+        return asset
+
+    def update(self, assets: {str: Asset}) -> {str: Asset}:
+        updated = {}
+
+        for asset in assets.values():
+            if asset.is_loaded():
+                for base in asset.rates.keys():
+                    self.load(asset=asset, base=base, time=datetime.datetime.utcnow())
+
+                updated[asset.id] = asset
+
+        return updated
