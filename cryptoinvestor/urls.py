650eba4ddc621333d1368c59b305f61ca559114f
Matej Vilk <matej.vilk@gmail.com>
2018-05-01 11:38:32 +0200

    Investments initial commit Fixes #11

--- cryptoinvestor/urls.py
+++ cryptoinvestor/urls.py
@@ -1,6 +1,6 @@
 import flask
 
-from cryptoinvestor.views import assets, index
+from cryptoinvestor.views import assets, index, investments
 
 
 def setup_urls(application: flask.Flask):
@@ -8,3 +8,6 @@ def setup_urls(application: flask.Flask):
     application.add_url_rule('/assets', view_func=assets.AssetsListView.as_view('assets_listview'))
     application.add_url_rule('/sell', view_func=assets.SellView.as_view('sell_view'))
     application.add_url_rule('/buy', view_func=assets.BuyView.as_view('buy_view'))
+    application.add_url_rule(
+        '/investments', view_func=investments.InvestmentsListView.as_view('investments_listview')
+    )
